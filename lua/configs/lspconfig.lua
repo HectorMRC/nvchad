1a002e0a390b6df6caaec5595441241eb9fac6e4
hector <hector.morales.carnice@gmail.com>
2024-06-02 01:19:33 +0200

    chore: add custom config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,23 +1,16 @@
--- EXAMPLE 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
+local lspconfig = require("lspconfig")
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
+-- Rust lsp config
+lspconfig.rust_analyzer.setup({
+    on_attach = function (client, bufnr)
+        local on_attach = require("nvchad.configs.lspconfig").on_attach
+        on_attach(client, bufnr)
+        -- disable LSP highlighting
+        client.server_capabilities.semanticTokensProvider = nil
+    end,
     capabilities = capabilities,
-  }
-end
-
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+    filetypes = {"rust"},
+    root_dir = lspconfig.util.root_pattern("Cargo.toml"),
+})
---
+++ lua/plugins/myplugins.lua
@@ -0,0 +1,44 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "nvchad.configs.lspconfig"
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    config = function ()
+      vim.g.rustfmt_autosave = 1
+      vim.g.rustfmt_emit_files = 1
+    end
+  },
+  {
+    "saecki/crates.nvim",
+    ft = {"rust", "toml"},
+    config = function(_, opts)
+      local crates = require "crates"
+      crates.setup(opts)
+      crates.show()
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "nvchad.configs.cmp"
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end
+  }
+}
+
+return plugins
