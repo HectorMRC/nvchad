983327e94ef0beeae15a16ee820e0fc25fe8b89a
hector <hector.morales.carnice@gmail.com>
2024-06-02 01:58:36 +0200

    chore(lsp): remove workaround

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,16 +1,12 @@
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
+local util = require("lspconfig.util")
 
 -- Rust lsp config
 lspconfig.rust_analyzer.setup({
-    on_attach = function (client, bufnr)
-        local on_attach = require("nvchad.configs.lspconfig").on_attach
-        on_attach(client, bufnr)
-        -- disable LSP highlighting
-        client.server_capabilities.semanticTokensProvider = nil
-    end,
+    on_attach = on_attach,
     capabilities = capabilities,
     filetypes = {"rust"},
-    root_dir = lspconfig.util.root_pattern("Cargo.toml"),
+    root_dir = util.root_pattern("Cargo.toml"),
 })
